9f1aa58fc42d1fc62987609e200852fce192fa92
unknown <hach.chp@gmail.com>
2025-08-10 09:30:47 +0800

    upload

---
+++ README.md
@@ -0,0 +1,36 @@
+# VeighNa框架的AData数据服务接口
+
+<p align="center">
+  <img src ="https://vnpy.oss-cn-shanghai.aliyuncs.com/vnpy-logo.png"/>
+</p>
+
+<p align="center">
+
+    <img src ="https://img.shields.io/badge/version-2025.8.1-blueviolet.svg"/>
+    <img src ="https://img.shields.io/badge/platform-windows|linux|macos-yellow.svg"/>
+    <img src ="https://img.shields.io/badge/python-3.10-blue.svg" />
+    <img src ="https://img.shields.io/github/license/vnpy/vnpy.svg?color=orange"/>
+
+</p>
+
+## 说明
+
+基于[adata](https://github.com/1nchaos/adata)开发的vnpy数据接口：
+
+## 安装
+
+安装环境推荐基于3.0.0版本以上的【[**VeighNa Studio**](https://www.vnpy.com)】。
+
+下载源代码后，解压后在cmd中运行：
+
+```
+pip install .
+```
+
+## 使用
+
+在 C:\\Users\\xxx.vntrader\vt\_setting.json vnpy 配置文件中修改：
+
+```json
+{"datafeed.name": "adata"}
+```
---
+++ vnpy_adata/adata_datafeed.py
@@ -0,0 +1,155 @@
+from datetime import datetime
+from typing import Dict, List, Optional, Callable
+from copy import deepcopy
+
+import adata
+from pandas import DataFrame
+
+from vnpy.trader.datafeed import BaseDatafeed
+from vnpy.trader.constant import Exchange, Interval
+from vnpy.trader.object import BarData, HistoryRequest
+from vnpy.trader.utility import round_to, ZoneInfo
+
+# 股票支持列表
+STOCK_LIST: List[Exchange] = [
+    Exchange.SSE,
+    Exchange.SZSE,
+    Exchange.BSE,
+]
+
+# 期货支持列表
+FUTURE_LIST: List[Exchange] = [
+    Exchange.CFFEX,
+    Exchange.SHFE,
+    Exchange.CZCE,
+    Exchange.DCE,
+    Exchange.INE,
+]
+
+# 交易所映射
+EXCHANGE_VT2EF: Dict[Exchange, str] = {
+    Exchange.CFFEX: "8",
+    Exchange.SHFE: "113",
+    Exchange.CZCE: "115",
+    Exchange.DCE: "114",
+    Exchange.INE: "142",
+    Exchange.GFEX: "225",
+    Exchange.SSE: "SSE",
+    Exchange.SZSE: "SZSE",
+}
+
+# 时间调整映射
+INTERVAL_ADJUSTMENT_MAP: Dict[Interval, int] = {
+    Interval.MINUTE: 1,
+    # Interval.MINUTE_5: 5,
+    # Interval.MINUTE_15: 15,
+    # Interval.MINUTE_30: 30,
+    Interval.HOUR: 60,
+    Interval.DAILY: 101,
+    Interval.WEEKLY: 102,
+    # Interval.MONTH: 103
+}
+
+# 中国上海时区
+CHINA_TZ = ZoneInfo("Asia/Shanghai")
+
+
+def to_adata_quote(exchange):
+    """选择股票"""
+    if exchange in STOCK_LIST:
+        datafeed = getattr(adata.stock, 'market')
+    else:
+        return None
+
+    return datafeed
+
+
+class AdataDatafeed(BaseDatafeed):
+    """Adata数据服务接口"""
+
+    def __init__(self):
+        """"""
+        self.inited: bool = False
+
+    def init(self, output: Callable = print) -> bool:
+        """初始化"""
+        if self.inited:
+            return True
+
+        self.inited = True
+
+        return True
+
+    def query_bar_history(self, req: HistoryRequest, output: Callable = print) -> Optional[List[BarData]]:
+        """查询k线数据"""
+        if not self.inited:
+            self.init(output)
+
+        symbol: str = req.symbol
+        exchange: Exchange = req.exchange
+        interval: Interval = req.interval
+        start: datetime = req.start.strftime("%Y-%m-%d")
+        end: datetime = req.end.strftime("%Y-%m-%d")
+
+        datafeed = to_adata_quote(exchange)
+
+        try:
+            d1: DataFrame = datafeed.get_market(
+                stock_code=symbol, k_type=1, start_date=start, end_date=end)
+        except IOError as ex:
+            output(f"发生输入/输出错误：{ex}")
+            return []
+
+        df: DataFrame = deepcopy(d1)
+
+        # 处理原始数据中的NaN值
+        df.fillna(0, inplace=True)
+        bar_keys: List[datetime] = []
+        bar_dict: Dict[datetime, BarData] = {}
+        data: List[BarData] = []
+
+        if df is not None:
+            for _, row in df.iterrows():
+                symbol = symbol
+                exchange = exchange
+
+                if interval.value == "d" or interval.value == 'w' or interval.value == 'm':
+                    dt: str = row["trade_date"]
+                    dt: datetime = datetime.strptime(dt, "%Y-%m-%d")
+                else:
+                    dt: str = row["日期"]
+                    dt: datetime = datetime.strptime(dt, "%Y-%m-%d %H:%M")
+
+                dt = dt.replace(tzinfo=CHINA_TZ)
+
+                volume = row['volume']
+                turnover = row['turnover_ratio']
+                open_interest = 0
+                open_price = row['open']
+                high_price = row['high']
+                low_price = row['low']
+                close_price = row['close']
+
+                bar: BarData = BarData(
+                    symbol=symbol,
+                    exchange=exchange,
+                    datetime=dt,
+                    interval=interval,
+                    volume=float(volume),
+                    open_price=round_to(open_price, 0.000001),
+                    high_price=round_to(high_price, 0.000001),
+                    low_price=round_to(low_price, 0.000001),
+                    close_price=round_to(close_price, 0.000001),
+                    turnover=float(turnover),
+                    open_interest=float(open_interest),
+                    gateway_name="AData",
+                )
+
+                bar_dict[dt] = bar
+
+            bar_keys: list = bar_dict.keys()
+            bar_keys = sorted(bar_keys, reverse=False)
+            for i in bar_keys:
+                data.append(bar_dict[i])
+
+        return data
